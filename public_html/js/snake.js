82313a88e7fe5e3f83020f49f70873f41d22c319
irbin ojeda <irbinojeda@gmail.com>
2015-02-06 11:30:24 -0800

    started to size the snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,18 +1,23 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenHeight;
 var screenWidth;
 
 gameInitialize ();
-gameDraw (); 
+gameDraw(); 
 
 function gameInitialize(){
-    var canvas = document.getElementById ("game-screen");
-    contextt = canvas.getContext ("2d");
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
     
-    screenwidth = window.innerWidth;
+    screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
+ 
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
     
 }
 
@@ -22,6 +27,25 @@ function gameLoop(){
 }
 
 function gameDraw() {
-    context.fillstyle = "rgb(92,245,78)";
-    context.fillRect (0,0, screenWidth, screenHeight);
+    console.log(screenWidth + " " + screenHeight);
+    context.fillStyle = "rgb(13,255,21)";
+    context.fillRect(0, 0, screenWidth , screenHeight);
+}
+
+function snakeInitialize () {
+    snake =  [];
+    snakeLength = 5;
+    sankeSize  = 20;
+    
+    for (var index = 0;index < snakeLength; index++ ) {
+        
+    }
+}
+
+function snakeDraw () {
+    
+}
+
+function snakeUpdate () {
+    
 }
